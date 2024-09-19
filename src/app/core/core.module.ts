a808a95d7ed012e5a4826b11fd2d1ded011dd5d6
Henry Cortez <henrystevencortez@yahoo.es>
2024-09-19 16:28:46 -0500

    Final clase 1

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,54 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { PageLoginComponent } from './page-login/page-login.component';
+import { LoginComponent } from './login/login.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    MenuComponent,
+    PageLoginComponent,
+    LoginComponent,
+  ],
   imports: [
     CommonModule,
-    
-  ]
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTooltipModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    HeaderComponent,
+    MenuComponent,
+    PageLoginComponent,
+  ],
 })
-export class CoreModule { }
+export class CoreModule {}
